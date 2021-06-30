ef91b16e8e9405be3d59906e233d28db9b202628
litsazib <roktimsazib92@gmail.com>
2021-06-30 12:12:39 +0600

    animation gsap

--- js/main.js
+++ js/main.js
@@ -24,21 +24,19 @@ function pointer() {
   });
 }
 
-document.addEventListener("click", function() {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
+
 
 window.addEventListener("scroll", function(e) {
-  const target = document.querySelector('.scroll');
-  const target1 = document.querySelector('.scroll1');
+  // const target = document.querySelector('.scroll');
+  // const target1 = document.querySelector('.scroll1');
   // const imageView = document.querySelector('.anims');
   var scrolled = window.pageYOffset;
   var rate = scrolled * -0.500;
   var rate1 = scrolled * -0.500;
   var imageViewRate = scrolled * -0.500;
   
-  target.style.transform = 'translate3d(0px,'+rate+'px,0px)';
-  target1.style.transform = 'translate3d(-50%,'+rate1+'px,0px)';
+  // target.style.transform = 'translate3d(0px,'+rate+'px,0px)';
+  // target1.style.transform = 'translate3d(-50%,'+rate1+'px,0px)';
   // imageView.style.transform = 'translate(0px'+imageViewRate+'px)';
 });
 

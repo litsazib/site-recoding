9bd5ef3829606ae43165216b4c65fb7f391e3616
litsazib <roktimsazib92@gmail.com>
2021-07-26 16:41:51 +0600

    replace pdf-20210716

--- js/main.js
+++ js/main.js
@@ -117,17 +117,10 @@ function pointer() {
 }
 
 window.addEventListener("scroll", function(e) {
-  // const target = document.querySelector('.scroll');
-  // const target1 = document.querySelector('.scroll1');
-  // const imageView = document.querySelector('.anims');
   var scrolled = window.pageYOffset;
   var rate = scrolled * -0.500;
   var rate1 = scrolled * -0.500;
   var imageViewRate = scrolled * -0.500;
-  
-  // target.style.transform = 'translate3d(0px,'+rate+'px,0px)';
-  // target1.style.transform = 'translate3d(-50%,'+rate1+'px,0px)';
-  // imageView.style.transform = 'translate(0px'+imageViewRate+'px)';
 });
 
 
